f993f16786f677cb4bf5970a181f965628e8b771
Christian Bueno <chris@f34.ec>
2021-06-12 21:48:19 -0500

    add message in newsletter2.css about not:(:placeholder-show)

--- newsletter.js
+++ newsletter.js
@@ -1,4 +1,8 @@
-let re = /ab+c/
-
-const matches = 'helllo@slack.com'.match('hel*o')
-console.log(matches +' '+ len(matches))
+let re = /hel*o/g;  //literal notaion, RegExp object
+// global flag g, returns all
+// default only the first
+// match(regexp object)
+// it is implicitly converted
+const matches = 'helllo@slackheo.gobhelo.comhelobyeyuhellokikohelllloyumalay'.match(re);
+console.log(matches +' '+ matches.length, typeof matches);
+console.log('hello 5');
